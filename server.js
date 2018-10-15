44393c808f4b2a169f6e951de737119e99e12542
Kavita Mane <kavita.mane@tcs.com>
2018-10-15 11:48:56 +0530

    make

--- server.js
+++ server.js
@@ -15,3 +15,23 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
